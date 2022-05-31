70c3f49e81f32ee20e71ac5b1cd6fc26b4e24a59
OsmaroB <osma204bonillamestizo@hotmail.com>
2022-05-31 16:42:19 -0600

    sadgasfghdf

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+ 
 const swaggerOptions = {
     swaggerDefinition:{
         info: {
